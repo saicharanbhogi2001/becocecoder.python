d8e9a731bf9c8cb609757f92a92734be6d0ac6d6
saicharanbhogi2001 <84128500+saicharanbhogi2001@users.noreply.github.com>
2021-05-13 16:05:14 +0530

    Add files via upload

---
+++ rev of mid num.py
@@ -0,0 +1,22 @@
+while 1:
+    n=int(input())
+    c=s=k=0
+    t=n
+    while n:
+        r=n%10
+        n=n//10
+        c+=1
+    c=s=c-1
+    n=t
+    p=n%10
+    q=n//(10**c)
+    while n:
+        r=n%10
+        n=n//10
+        if c==s:
+            r=q
+        elif c==0:
+            r=p
+        k=k*10+r
+        c-=1
+    print(k)
